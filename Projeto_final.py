d0aa1ab2a1c33d8903880a3d8797265193ab3adf
Theo Barbara <theobarbara@terra.com.br>
2018-10-30 17:45:54 -0300

    Código aprimorado, fundo rodando a tela

--- Projeto_final.py
+++ Projeto_final.py
@@ -40,12 +40,18 @@ todos_amigos = pygame.sprite.Group()
 todos_inimigos = pygame.sprite.Group()
 tela = pygame.display.set_mode((800, 600), 0, 32)
 fundo = pygame.image.load("imagem_fundo.png").convert()
+background_size = fundo.get_size()
 per_1 = Personagem(["menino correndo/Run__000.png", "menino correndo/Run__001.png","menino correndo/Run__002.png","menino correndo/Run__003.png","menino correndo/Run__004.png","menino correndo/Run__005.png","menino correndo/Run__006.png", "menino correndo/Run__007.png","menino correndo/Run__008.png","menino correndo/Run__009.png"], 300, 350)
 todos_amigos.add(per_1)
 relogio = pygame.time.Clock()
 
 
 game_run = True
+w,h = background_size
+x = 0
+y = 0
+x1 = w
+y1 = 0
 
 
 pygame.display.set_caption('Foge do Touro!')
@@ -54,12 +60,26 @@ while game_run:
     for event in pygame.event.get():
       if event.type == QUIT:
           game_run = False
-    tela.blit(fundo, (0, 0))
+#    tela.blit(fundo, (0, 0))
+    x1 -= 10
+    x -= 10
+    tela.blit(fundo, (x, y))
+    tela.blit(fundo, (x1, y1))
+    if x < -w:
+        x = w
+    if x1 < -w:
+        x1 = w
+#    pygame.display.update()
+    
+    
+    
     todos_amigos.draw(tela)
     todos_inimigos.draw(tela)
     todos_amigos.update()
     todos_inimigos.update()
+    pygame.display.flip()
     pygame.display.update()
+        
                 
 pygame.display.quit()
 
