1bc5389e2dbc7a841eb57469101d4f601f69c584
andreamt2 <38547074+andreamt2@users.noreply.github.com>
2018-11-08 16:33:10 -0200

    Add files via upload

--- Projeto_final.py
+++ Projeto_final.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from pygame.locals import *
 import numpy as np
-import random
+from random import randrange
 
 inicio = pygame.init()
 
@@ -29,8 +29,9 @@ class Personagem(pygame.sprite.Sprite):
     def damage(self, touro, personagem, dano):
         for enemy in touro and pygame.sprite.collide(personagem, touro, False):
             enemy.vida -= dano
+            
     def jump(self):
-        self.v_y = -10
+        self.v_y = -14
         
         print(self.rect.y)
             
@@ -46,7 +47,7 @@ class Personagem(pygame.sprite.Sprite):
             self.rect.y = 350
             self.v_y = 0
             
-class Personagem2(pygame.sprite.Sprite):
+class Dinossauro(pygame.sprite.Sprite):
     
     def __init__(self, sprite, pos_x, pos_y):
         self.atual = 0
@@ -63,14 +64,6 @@ class Personagem2(pygame.sprite.Sprite):
     def vel(self, velocidade):
         self.v_x = velocidade
     
-    def vida(self, life):
-        self.vida = life
-    
-    def damage(self, touro, personagem, dano):
-        for enemy in touro and pygame.sprite.collide(personagem, touro, False):
-            enemy.vida -= dano
-   
-            
     def update(self):
         self.atual += 1
         if self.atual >= len(self.imagens):
@@ -110,11 +103,10 @@ background_size = fundo.get_size()
 
 per_1 = Personagem(["menino correndo/Run__000.png", "menino correndo/Run__001.png","menino correndo/Run__002.png","menino correndo/Run__003.png","menino correndo/Run__004.png","menino correndo/Run__005.png","menino correndo/Run__006.png", "menino correndo/Run__007.png","menino correndo/Run__008.png","menino correndo/Run__009.png"], 400, 350)
 #per_1_j = Personagem(["menino pulando/Jump__000", "menino pulando/Jump__001", "menino pulando/Jump__002", "menino pulando/Jump__003", "menino pulando/Jump__004", "menino pulando/Jump__005", "menino pulando/Jump__006", "menino pulando/Jump__007", "menino pulando/Jump__008", "menino pulando/Jump__009"])
-dino = Personagem(["dinossauro/Run (1).png", "dinossauro/Run (2).png", "dinossauro/Run (3).png", "dinossauro/Run (4).png", "dinossauro/Run (5).png", "dinossauro/Run (6).png", "dinossauro/Run (7).png", "dinossauro/Run (8).png"], 100, 350)
-dino = Personagem2(["dinossauro/Run (1).png", "dinossauro/Run (2).png", "dinossauro/Run (3).png", "dinossauro/Run (4).png", "dinossauro/Run (5).png", "dinossauro/Run (6).png", "dinossauro/Run (7).png", "dinossauro/Run (8).png"], 10, 280)
+dino = Dinossauro(["dinossauro/Run (1).png", "dinossauro/Run (2).png", "dinossauro/Run (3).png", "dinossauro/Run (4).png", "dinossauro/Run (5).png", "dinossauro/Run (6).png", "dinossauro/Run (7).png", "dinossauro/Run (8).png"], 10, 280)
 obst = obstaculos(["obstaculos/pedra.png", "obstaculos/tronco.png", "obstaculos/caixa.png"], 500, 270)
+
 todos_amigos.add(per_1)
-objetos = ["obstaculos/pedra.png", "obstaculos/tronco.png", "obstaculos/caixa.png"]
 todos_inimigos.add(dino)
 todos_obstaculos.add(obst)
 relogio = pygame.time.Clock()
@@ -141,12 +133,6 @@ while game_run:
     x1 -= 10
     x -= 10
     tela.blit(fundo, (x, y))
-    for n in range(2):
-        img = random.choice(obstaculos.obst)
-        img.draw(tela)
-    
-        
-        
     tela.blit(fundo, (x1, y1))
     if x < -w:
         x = w
@@ -154,16 +140,13 @@ while game_run:
         x1 = w
     
     
-    
-    
     todos_amigos.draw(tela)
+    
     todos_obstaculos.update()
     todos_obstaculos.draw(tela)
     todos_amigos.update()
     todos_inimigos.update()
     todos_inimigos.draw(tela)
-    selec_1.draw(tela)
-    selec_2.draw(tela)
     pygame.display.flip()
     pygame.display.update()
         
