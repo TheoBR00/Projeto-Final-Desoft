58cd206fb9d1954efed1dd913627372ed76edc33
CEODINIZ <38334385+CEODINIZ@users.noreply.github.com>
2018-12-04 18:19:57 -0200

    nova versao

--- timer_na_tela.py
+++ timer_na_tela.py
@@ -1,19 +1,19 @@
-cronometro = 0
-timer = True
+pygame.display.set_caption('Foge do Dinossauro!')
 font = pygame.font.SysFont('assets/swiss911.ttf', 40)
-        
+while game_run:
+    #tempo = str("%.2f" % cronometro)
+    #tela.blit(font.render(tempo, True, (0, 0, 0)), (550,25))
+    #pygame.display.update()
+    if timer:
         tempo = str("%.2f" % cronometro)
         tela.blit(font.render(tempo, True, (0, 0, 0)), (550,25))
-        pygame.display.update()   
-        if timer:
-            
-                font = pygame.font.SysFont('Consolas', 50)      
-                total = str("%.2f" % cronometro)
-                tela.blit(font.render(tempo, True, (0, 0, 0)), (50,50))
-                pygame.display.update()
+        font = pygame.font.SysFont('Consolas', 50)      
+        total = str("%.2f" % cronometro)
+        #tela.blit(font.render(tempo, True, (0, 0, 0)), (50,50))
+        pygame.display.update()
                 
-            if not game_run
-               #cronometro = Pause  
-               timer = False
+        if not game_run:
+            #cronometro = Pause  
+            timer = False
             if timer:
-        cronometro += 1/30
+                cronometro += 1/30
